072f3ed083c349b7b22f2472657a875000648711
Geo <gravyplaya@gmail.com>
2018-07-17 21:36:41 -0700

    Updated to use cryptoss template for bitcoin

--- src/ninja.paperwallet.js
+++ src/ninja.paperwallet.js
@@ -124,13 +124,13 @@ ninja.wallets.paperwallet = {
 		var walletBackgroundUrl = "wallets/" + janin.selectedCurrency.name.toLowerCase() + ".png";
 
 		var walletHtml =
-							"<div class='coinIcoin'> <img id='coinImg' src='" + coinImgUrl + "' alt='currency_logo' /></div><div class='artwallet' id='artwallet" + i + "'>" +
+							"<div class='artwallet' id='artwallet" + i + "'>" +
 								"<img id='papersvg" + i + "' class='papersvg' src='" + walletBackgroundUrl + "' />" +
 								"<div id='qrcode_public" + i + "' class='qrcode_public'></div>" +
 								"<div id='qrcode_private" + i + "' class='qrcode_private'></div>" +
 								"<div class='btcaddress' id='btcaddress" + i + "'></div>" +
 								"<div class='" + keyelement + "' id='" + keyelement + i + "'></div>" +
-								"<div class='paperWalletText'><img class='backLogo' src='" + coinImgUrl + "' alt='currency_logo' />" + ninja.translator.get("paperwalletback") + "</div>" +
+								// "<div class='paperWalletText'><img class='backLogo' src='" + coinImgUrl + "' alt='currency_logo' />" + ninja.translator.get("paperwalletback") + "</div>" +
 							"</div>";
 		return walletHtml;
 	},
